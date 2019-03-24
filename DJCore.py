c24e63e42bcb638239e2fb9b7d846f429897956c
MASERBOSS <47029521+MASERBOSS@users.noreply.github.com>
2019-03-24 21:53:19 +0530

    Update DJCore.py

--- DJCore.py
+++ DJCore.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 
 bot = commands.AutoShardedBot(case_insensitive=True, command_prefix=commands.when_mentioned_or(os.getenv('PREFIX')))
 bot.remove_command('help')
-bot.initials = ('modules.misc', 'modules.music', 'modules.handler', 'modules.owner')
+bot.initials = ('modules.misc', 'modules.music', 'modules.handler', 'modules.owner', 'jishaku')
 bot.owner = int(os.getenv('OWNER'))
 bot.color = int(os.getenv('COLOR'), 16)
 
@@ -15,7 +15,7 @@ async def _bot_protection(ctx):
 @bot.event
 async def on_ready():
     print(f'Bot is ready! Logged as in: {bot.user}')
-    await bot.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.watching, name=f"your move | {os.getenv('PREFIX')}help"))
+    await bot.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.watching, name=f"you | {os.getenv('PREFIX')}help"))
 
 if __name__ == "__main__":
     for extension in bot.initials:
